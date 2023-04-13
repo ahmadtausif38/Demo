99b2c9b15f2dcae4b157325a852374e2baecfa27
Tausif <ahmadtausif38@gmail.com>
2023-04-13 16:14:59 +0530

    First Modification

--- ContactServiceApplication.java
+++ ContactServiceApplication.java
@@ -4,7 +4,7 @@ public class ContactServiceApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Working....");
+		System.out.println("Working properly....");
 	}
 	
 	
